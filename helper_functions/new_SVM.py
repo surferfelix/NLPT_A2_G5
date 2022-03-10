5ba56ab8507c9ee88726fd566bb96dea884cf3fd
surferfelix <felixdenheijer@hotmail.com>
2022-03-10 21:59:08 +0100

    Feature pipe working

--- helper_functions/new_SVM.py
+++ helper_functions/new_SVM.py
@@ -5,24 +5,37 @@ import pandas as pd
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import classification_report, confusion_matrix
+from extract_features import get_embedding_pipe
 
-def extract_features_and_labels(file_path, selected_features, label):
+def extract_features_and_labels(file_path, selected_features, label, embedding_path = ''):
     """Extract a set of features and gold labels from file."""
 
     features = []
     labels = []
-
+    if 'embedding' in selected_features:
+        embedding = True
+    else:
+        print(f'not using embeddings for {file_path}')
+        embedding = False
     with open(file_path, 'r', encoding='utf-8') as infile:
         # restval specifies value to be used for missing values
         reader = csv.DictReader(infile, restval='', delimiter='\t', quotechar='|')
         for row in reader:
             feature_dict = {}
             for feature_name in selected_features:
-                if row[feature_name]:  # if this feature exists
-                    feature_dict[feature_name] = row[feature_name]
-            features.append(feature_dict)
-            labels.append(row[label])
+                if not feature_name == 'embedding':
+                    if row[feature_name]:  # if this feature exists
+                        feature_dict[feature_name] = row[feature_name]
+                features.append(feature_dict)
+                labels.append(row[label])
+    if embedding and embedding_path:
+        print('Using Embeddings')
+        vector_reps = get_embedding_pipe(file_path, embedding_path)# Here i need to add model_path parameter, so thsi is relevant for main
+        print(vector_reps)
+    else:
+        print(embedding_path, file_path, label, embedding_path)
 
+    # assert all([embedding == False, bool(embedding_path) == True]), 'Please add "embedding" to your selected features'
     return features, labels
 
 def create_classifier(train_features, train_labels):
@@ -37,12 +50,14 @@ def create_classifier(train_features, train_labels):
     return classifier, vec
 
 
-def get_predicted_and_gold_labels(test_path, vectorizer, classifier, selected_features, label):
+def get_predicted_and_gold_labels(test_path, vectorizer, classifier, selected_features, label, embedding_path = ''):
     """Vectorize test features and get predictions."""
 
     # we use the same function as above (guarantees features have the same name and form)
-    test_features, gold_labels = extract_features_and_labels(test_path, selected_features, label)
-    
+    if embedding_path:
+        test_features, gold_labels = extract_features_and_labels(test_path, selected_features, label, embedding_path)
+    if not embedding_path:
+        test_features, gold_labels = extract_features_and_labels(test_path, selected_features, label)
     # we need to use the same fitting as before, so now we only transform the current features according to this
     # mapping (using only transform)
     test_features_vectorized = vectorizer.transform(test_features)
@@ -51,16 +66,18 @@ def get_predicted_and_gold_labels(test_path, vectorizer, classifier, selected_fe
 
     return predictions, gold_labels
 
-def run_classifier_and_return_predictions_and_gold(train_path, test_path, selected_features, label, name):
+def run_classifier_and_return_predictions_and_gold(train_path, test_path, selected_features, label, name, embedding_path = ''):
     """Run classifier and get predictions using default parameters or cross validation."""
 
-    train_features, train_labels = extract_features_and_labels(train_path, selected_features, label) # train_features, train_labels = 
+    if not embedding_path:
+        train_features, train_labels = extract_features_and_labels(train_path, selected_features, label)
+    elif embedding_path:
+        train_features, train_labels = extract_features_and_labels(train_path, selected_features, label, embedding_path)
 
     classifier, vectorizer = create_classifier(train_features, train_labels)
 
     predictions, gold_labels = get_predicted_and_gold_labels(test_path, vectorizer, classifier, 
                                                              selected_features, label)
-
     
     list_dict = {'predict':predictions, 'gold':gold_labels} 
     df = pd.DataFrame(list_dict) 
@@ -74,16 +91,17 @@ def main(paths=None) -> None:
 
     if not paths:  # if no paths are passed to the function through the command line
         
-        paths = ['../feature_data/final_train_with_feature.tsv', # FeatureFile
-                        '../feature_data/final_te_with_feature.tsv']  # Automatic Evaluation
+        paths = ['../feature_data/mini_final_train_with_feature.tsv', # FeatureFile
+                        '../feature_data/mini_final_te_with_feature.tsv', '/Volumes/Samsung_T5/Text_Mining/Models/enwiki_20180420_100d.txt']  # Embedding_Path
 
     # change the features for different tasks 
     # 0 = token, 1 = spacy_lemma, 2 = head, 3= path, 4 = '?', 5 = '?', 6 = POS, 7 = type 8 = ?, 
     # 15 = GOLD PRED, # 16 = GOLD ARG
     selected_features = ['tokens', 'lemmas', 'heads', 'named_entities', 'sentences_for_token', 'Prev_pos', 'Next_pos',
-    '4', '5', '6', '8', '9']
+    '4', '5', '6', '8', '9', 'embedding']
     train_path = paths[0]
     test_path = paths[1]
+    embedding_path = paths[2]
 
     labels_name = {"arguments": "argument_classification", "gold_predicate_binary":"pred_identification", 
                "gold_arguments_binary":"arg_identification"}
@@ -91,15 +109,14 @@ def main(paths=None) -> None:
     for label, name in labels_name.items():
         print(f'running {name}')
         run_classifier_and_return_predictions_and_gold(train_path, test_path, selected_features, 
-                                                       label, name)   
+                                                       label, name, embedding_path)   
 
 
 # to use in the main.py
-# TODO: Add the default selected features when they are in their final version
-def run_model(input_train_path: str, input_test_path: str, input_selected_features: list, input_label: str, input_name: str):
+def run_model(input_train_path: str, input_test_path: str, input_selected_features: list, input_label: str, input_name: str, embedding_path = ''):
 
     run_classifier_and_return_predictions_and_gold(input_train_path, input_test_path, input_selected_features, 
-                                                       input_label, input_name) 
+                                                    input_label, input_name, embedding_path) 
 
 
 if __name__ == '__main__':
